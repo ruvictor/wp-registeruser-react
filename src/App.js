8554928c698d6c021231812204f1c634b4c51741
Victor <rusuvictor30@gmail.com>
2019-05-07 08:04:18 -0400

    Submition Done

--- src/App.js
+++ src/App.js
@@ -10,25 +10,38 @@ class App extends Component {
       password: ''
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-// 30:54
+
   handleChange(e){
     let target = e.target;
-    let value = target.type === 
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+    console.log('Datele');
+    console.log(this.state);
   }
 
   render(){
     return (
       <div className="App">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
-          <input value={this.state.email} onChange={this.handleChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <input name="email" value={this.state.email} onChange={this.handleChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
           <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
-          <input value={this.state.password} onChange={this.handleChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          <input name="password" value={this.state.password} onChange={this.handleChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
